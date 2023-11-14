3c2e3fb57509f90f5af83be71b04a95fc688ecf6
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2023-11-14 14:56:30 +0000

    Adicionar os métodos construtores da classe Pessoa

--- src/com/school/Pessoa.java
+++ src/com/school/Pessoa.java
@@ -7,4 +7,25 @@ public class Pessoa {
     private String apelido;
     // idade da Pessoa
     private int idade;
+
+    /**
+     * Construtor default
+     * só é utilizado quando não existir mais nenhum.
+     */
+    public Pessoa(){
+
+    }
+
+    /**
+     * Construtor completo
+     * Permite criar os objetos com todos os atributos
+     * @param nome recebe o nome da Pessoa
+     * @param apelido recebe o apelido da Pessoa
+     * @param idade recebe a idade da Pessoa
+     */
+    public Pessoa(String nome, String apelido, int idade){
+        this.nome = nome;
+        this.apelido = apelido;
+        this.idade = idade;
+    }
 }
