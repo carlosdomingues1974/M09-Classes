b28fe16bbf72e18dc7363fa1f80ae56efd8d327b
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2023-11-14 14:33:58 +0000

    Adicionar os atributos da classe Pessoa

--- src/com/school/Pessoa.java
+++ src/com/school/Pessoa.java
@@ -1,4 +1,10 @@
 package com.school;
 
 public class Pessoa {
+    // nome da Pessoa
+    private String nome;
+    // apelido da Pessoa
+    private String apelido;
+    // idade da Pessoa
+    private int idade;
 }
